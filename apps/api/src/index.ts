8e8b6f6f9e24c8029a4ea950ab70aafe5658ebea
DevAlexIon <mr_alexandruion@icloud.com>
2026-03-01 19:56:07 +0200

    chore: add typecheck script and fix turbo pipeline config

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import { waitlistRouter } from './routes/waitlist'
+import { waitlistRouter } from './routes/waitlist.js'
 
 dotenv.config()
 
